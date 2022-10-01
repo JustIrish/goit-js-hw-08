7d841a41f5464dffd4952bca3e288deadc0f2498
Irina Khorunz <irahorunz87@gmail.com>
2022-10-01 14:12:27 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,20 @@
 import Player from '@vimeo/player';
-
+const throttle = require('lodash.throttle');
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', saveTime);
+player.on('timeupdate', throttle(saveTime, 1000));
 
 function saveTime(data) {
-  localStorage.setItem('videoplayer-current-time', data);
-  console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }
 
 player.on('play', onPlay);
 
 function onPlay(data) {
   const currentTime = localStorage.getItem('videoplayer-current-time');
-  if (data !== currentTime) {
+  console.log(currentTime);
+  if (data.seconds !== currentTime) {
     player.setCurrentTime(currentTime);
   }
 }
