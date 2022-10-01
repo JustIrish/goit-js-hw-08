3d4cb868678f1fe0b5021a4a7464aa52e799039b
Irina Khorunz <irahorunz87@gmail.com>
2022-10-01 13:22:41 +0300

    add vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', saveTime);
+
+function saveTime(data) {
+  localStorage.setItem('videoplayer-current-time', data);
+  console.log(data);
+}
+
+player.on('play', onPlay);
+
+function onPlay(data) {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (data !== currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
