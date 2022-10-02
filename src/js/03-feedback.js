b00cafb11cdc49b468ce72331591ee2bfd739186
Irina Khorunz <irahorunz87@gmail.com>
2022-10-02 20:05:08 +0300

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+const throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', onFormSubmit);
+fillFormField();
+
+const data = {};
+
+function handleInput(evt) {
+  data[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!savedData?.email || !savedData.message) {
+    alert('All fields must be filled!!!');
+  } else {
+    console.log(savedData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+function fillFormField() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    form.elements.email.value = savedData.email;
+    form.elements.message.value = savedData.message;
+  }
+}
