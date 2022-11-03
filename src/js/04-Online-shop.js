f72a8677dcff0eff794a9166973268b99932b2fb
Irina Khorunz <irahorunz87@gmail.com>
2022-11-03 11:51:13 +0200

    add Online shop

---
+++ src/js/04-Online-shop.js
@@ -0,0 +1,64 @@
+import { instruments } from './card-items';
+
+const STORAGE_KEY = 'product-to-basket';
+const list = document.querySelector('.list-card');
+const openModalBtn = document.querySelector('[data-modal-open]');
+const basketList = document.querySelector('.list-basket');
+const basket = [];
+
+const markup = instruments
+  .map(
+    ({ id, img, name, price }) => ` <li class="card-item js-item" data-id=${id}>
+    <img src = "${img}" alt = "${name}"/>
+        <div class="disc"><h2 class="title">${name}</h2>
+        <p>${price}</p>
+        <button class="js-add-btn">Add</button>
+        <button>Wish list</button></div>
+      </li>`
+  )
+  .join('');
+
+list.insertAdjacentHTML('beforeend', markup);
+
+list.addEventListener('click', onItem);
+openModalBtn.addEventListener('click', fillBasket);
+
+function onItem(e) {
+  if (e.target.classList.contains('js-add-btn')) {
+    const currentProd = e.target.closest('.js-item');
+    const currentId = Number(currentProd.dataset.id);
+    const toBasket = instruments.find(({ id }) => id === currentId);
+
+    const prodInBasket = basket.find(({ id }) => id === currentId);
+    if (!prodInBasket) {
+      toBasket.qty = 1;
+      toBasket.totalSum = toBasket.price;
+      basket.push(toBasket);
+    } else {
+      prodInBasket.qty += 1;
+      prodInBasket.totalSum = prodInBasket.qty * prodInBasket.price;
+    }
+    // console.log(basket);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(basket));
+    return;
+  }
+}
+
+function fillBasket() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(savedData);
+  if (!savedData) {
+    return;
+  } else {
+    const markup = savedData
+      .map(
+        ({ img, name, price }) => ` <li class="list-basket-item">
+    <img src = "${img}" alt = "${name}" width="200"/>
+        <div><h2 class="title">${name}</h2>
+        <p>${price}</p>
+      </li>`
+      )
+      .join('');
+    basketList.insertAdjacentHTML('beforeend', markup);
+  }
+}
---
+++ src/js/card-items.js
@@ -0,0 +1,38 @@
+export const instruments = [
+  {
+    id: 1,
+    img: 'https://static.dnipro-m.ua/cache/products/1761/catalog_origin_132283.jpg',
+    name: 'Молоток',
+    price: 300,
+  },
+  {
+    id: 2,
+    img: 'https://static.dnipro-m.ua/cache/products/2784/catalog_origin_215533.jpg',
+    name: 'Пасатижі',
+    price: 400,
+  },
+  {
+    id: 3,
+    img: 'https://static.dnipro-m.ua/cache/products/2023/catalog_origin_200763.jpg',
+    name: 'Рівень',
+    price: 500,
+  },
+  {
+    id: 4,
+    img: 'https://static.dnipro-m.ua/cache/products/5225/catalog_origin_214630.jpg',
+    name: 'Шуруповерт',
+    price: 1500,
+  },
+  {
+    id: 5,
+    img: 'https://static.dnipro-m.ua/cache/products/5098/catalog_origin_195568.jpg',
+    name: 'Перфоратор',
+    price: 3000,
+  },
+  {
+    id: 6,
+    img: 'https://takida.com.ua/uploads/shop/products/large/3daf82b3794e6e69f37e03dee8b653bc.jpg',
+    name: 'Болгарка',
+    price: 1400,
+  },
+];
