0b6e306cb3055abc2132b01e8d117b292b68a989
Irina Khorunz <irahorunz87@gmail.com>
2022-11-03 19:43:26 +0200

    Update modal basket

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,13 +4,31 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     body: document.querySelector('body'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', onBackdropClick);
 
   function toggleModal() {
+    window.addEventListener('keydown', onEscPress);
     refs.modal.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
+    if (refs.modal.classList.contains('is-hidden')) {
+      window.removeEventListener('keydown', onEscPress);
+    }
+  }
+
+  function onBackdropClick(evt) {
+    if (evt.currentTarget === evt.target) {
+      toggleModal();
+    }
+  }
+
+  function onEscPress(evt) {
+    if (evt.key === 'Escape') {
+      toggleModal();
+    }
   }
 })();
